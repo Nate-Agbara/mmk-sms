44a996a3af9d8dee657a596598b80d76d51c5d50
Nate-Agbara <Agbaraabah1>
2022-03-11 13:02:49 +0100

    dbchangelog

--- src/main/resources/dbchangelog/db/v1.2_AddAccount.sql
+++ src/main/resources/dbchangelog/db/v1.2_AddAccount.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO account (auth_id, username) VALUES ('20S0KPNOIM',	'azr1');
+INSERT INTO account (auth_id, username) VALUES ('20S0KPNOIM',	'$2a$12$D9omJAeJbSBwRYx21y/Ri.W36zMsZPYOb3siByJpFO0z7pBx.bSC2');
 INSERT INTO account (auth_id, username) VALUES ('54P2EOKQ47',	'azr2');
 INSERT INTO account (auth_id, username) VALUES ('9LLV6I4ZWI',	'azr3');
 INSERT INTO account (auth_id, username) VALUES ('YHWE3HDLPQ',	'azr4');
